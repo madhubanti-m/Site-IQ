8d1577c40fa2e0e6327088eb680591b16e656397
madhubanti-m <madhubantimukherjee@gmail.com>
2026-04-11 20:54:38 +0530

    Updated NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,7 +30,7 @@ export default function NavBar({ active, onNavigate }: NavBarProps) {
           className="flex items-center"
         >
           <img
-            src="/ScrapeIQ-2.png"
+            src="/ScrapeIQ-2 copy.png"
             alt="ScrapeIQ"
             style={{ height: "34px", width: "auto", objectFit: "contain" }}
           />
